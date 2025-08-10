3662b7a3420c1967b1e2f6416410dffbd82acc35
SmdZubair0 <smohammedzubair0@gmail.com>
2025-08-10 18:14:55 +0530

    added cors

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from src.core.config import settings
 from src.api.DataLoader import app as dataloader_router
 from src.api.TextSummarizer import app as summarizer_router
 from src.api.Query import app as query_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.allow_origins,
+    allow_credentials=settings.allow_credentials,
+    allow_methods=settings.allow_methods,
+    allow_headers=settings.allow_headers,
+)
 
 app.include_router(summarizer_router, prefix="/summarize")
 app.include_router(dataloader_router, prefix="/load")
