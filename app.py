f25ef25fce2db95b7be5b9729526c0d40356379f
Shaik Mohammed Zubair <108988964+SmdZubair0@users.noreply.github.com>
2025-08-15 20:45:30 +0530

    Updated route

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ import uvicorn
 # from src.main import app
 
 if __name__ == "__main__":
-    uvicorn.run("src.main:app", host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
